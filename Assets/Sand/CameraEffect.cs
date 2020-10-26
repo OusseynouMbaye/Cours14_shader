ea23d75c7b89f86d6dd58ea09205df46b2396482
ouzinem@hotmail.com <ouzinem@hotmail.com>
2020-10-26 15:54:58 -0400

    Création d’un post Processing image shader

---
+++ Assets/Sand/CameraEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+
+public class CameraEffect : MonoBehaviour
+{
+    Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/NewImageEffectShader"));
+    }
+     private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source,destination, material);
+    }
+}
